558d740806a53f9092ae6646a6edf43e894c6fdf
tiago <tiago@DESKTOP-00HI8IK>
2023-11-03 21:17:27 -0500

    Agregar archivos de proyecto.

---
+++ PRACTICA 2 U3 5/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace PRACTICA_2_U3_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(103, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(575, 33);
+            label1.TabIndex = 0;
+            label1.Text = "Genera los números del 1 al 20 con While";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightGoldenrodYellow;
+            button1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(176, 188);
+            button1.Name = "button1";
+            button1.Size = new Size(146, 68);
+            button1.TabIndex = 1;
+            button1.Text = "Imprime en\r\npantalla\r\n";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.LightGoldenrodYellow;
+            listBox1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 24;
+            listBox1.Location = new Point(473, 103);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(188, 292);
+            listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(192, 255, 192);
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ActiveCaptionText;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private ListBox listBox1;
+    }
+}
